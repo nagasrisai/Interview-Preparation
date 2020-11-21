4237d73d9842e38e359901ad977a478002bfb438
nagasrisai <59650078+nagasrisai@users.noreply.github.com>
2020-11-21 12:49:20 +0530

    Update array.cpp

--- array.cpp
+++ array.cpp
@@ -1,106 +1,140 @@
 #include<bits/stdc++.h>
 
-using namespace  std;
+using namespace std;
 
-void insert_at_beginning(int arr[], int *n, int ele) {
-    *n++;
+void insert_at_beginning (int arr[], int *n, int size, int ele) {
     int i;
+
 
     for(i=*n; i>=0; i--) {
-        arr[i]=arr[i-1];
-
+        arr[i+1]=arr[i];
     }
-    arr[i]=ele;
-}
+    arr[0]=ele;
 
-void insert_at_middle (int arr[], int *n, int ele, int pos ) {
     *n++;
-    int i;
-    for(i=*n; i>=pos; i--) {
-        arr[i]=arr[i-1];
-    }
-    arr[i]=ele;
-}
-
-void insert_at_end (int arr[], int *n, int ele) {
-    arr[*n++]=ele;
-}
-
-void delete_at_beginning (int arr[],int *n) {
-    int i;
-    for(int i=0; i<*n; i++) {
-        arr[i]=arr[i-1];
-    }
-    *n--;
 
 }
 
-void delete_at_middle (int arr[], int *n,  int pos) {
-
-    if(pos>*n) {
+void insert_at_middle (int arr[], int *n, int size, int ele, int pos) {
+    if(*n<pos) {
         try {
-            throw "Index out of bound range error";
+            throw "out of bound index";
         }
-        catch (const char* error) {
-            cout<<error<<" ";
+        catch (char const* error) {
+            cout<<error<<"\n";
+
         }
     }
 
     else {
 
-        for(int i=pos-1; i<*n; i++) {
-            arr[i]=arr[i+1];
+        for (int i = *n; i >pos; i--) {
+            arr[i + 1] = arr[i];
         }
+        arr[pos - 1] = ele;
 
+        *n++;
+    }
+}
+
+void insert_at_end (int arr[], int *n, int size, int ele) {
+
+    arr[*n++]=ele;
+}
+
+void delete_at_begining(int arr[], int *n , int size) {
+    int i=0;
+    for(i=0; i<*n; i++) {
+        arr[i]=arr[i+1];
     }
 
+    *n--;
 }
 
-void delete_at_end (int arr[], int *n) {
+void delete_at_middle (int arr[], int *n, int size, int pos) {
+
+    for(int i=pos-1; i<*n-1; i++) {
+        arr[i]=arr[i+1];
+    }
 
     *n--;
-
 }
 
-void search_for_element (int arr[], int n, int ele) {
+void delete_at_end(int arr[], int *n, int size) {
+    *n--;
+}
+
+void search_for_element (int arr[], int n, int size, int ele) {
+
     for(int i=0; i<n; i++) {
         if(arr[i]==ele) {
             cout<<i<<" ";
         }
     }
+
 }
-int main(){
-    int n=3;
-    int arr[n]={8,9,10};
+int main() {
 
-    insert_at_beginning(arr, &n, 1);
+    int size=6;
+
+    int arr[50]={0,2,3,4,5,6};
+
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    insert_at_beginning(arr, &n, size, 1);
+    size++;
+
+    insert_at_beginning(arr, &n, size, 7);
+    size++;
+
+    insert_at_beginning(arr, &n, size, 8);
+    size++;
+
+    insert_at_beginning(arr, &n,size,9);
+    size++;
+
+    insert_at_beginning(arr,&n,size,10);
+    size++;
+
+    insert_at_middle(arr, &n, size, 10, 3);
+    size++;
+
+    delete_at_begining(arr, &n, size);
+
+    delete_at_middle(arr, &n, size, 2);
+
+    delete_at_end(arr,&n, size);
+
+    search_for_element(arr,n,size,9);
+
+    cout<<arr[0]<<"\n";
+
+    cout<<arr[1]<<"\n";
+
+    cout<<arr[2]<<"\n";
+
+    cout<<arr[3]<<"\n";
+
+    cout<<arr[4]<<"\n";
+
+    cout<<arr[5]<<"\n";
+
+    cout<<arr[6]<<"\n";
+
+    cout<<arr[7]<<"\n";
+
+    cout<<arr[8]<<"\n";
+
+    cout<<arr[9]<<"\n";
+
+    cout<<arr[10]<<"\n";
+
+    cout<<size<<"size"<<"\n";
 
 
-    insert_at_beginning(arr, &n, 2);
 
-
-    insert_at_beginning(arr, &n, 3);
-
-
-    insert_at_beginning(arr, &n, 4);
-
-
-    insert_at_middle(arr, &n,5,3);
-    assert(3<n);
-
-    insert_at_end(arr, &n,11);
-
-    delete_at_beginning(arr,&n);
-
-    delete_at_end(arr,&n);
-
-    delete_at_middle(arr,&n,2);
-
-    search_for_element(arr,n,8);
-
-    for(int i=0; i<n; i++) {
-        cout<<arr[i]<<" ";
-    }
+    cout<<n<<"total"<<"\n";
 
     return 0;
+
 }
