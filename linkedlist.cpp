3d76f755351ea98b0d1308413a3e517be1835782
nagasrisai <59650078+nagasrisai@users.noreply.github.com>
2020-11-23 18:39:10 +0530

    Update linkedlist.cpp

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -1,5 +1,7 @@
 #include<bits/stdc++.h>
+
 using namespace std;
+
 struct node {
     int data;
 
@@ -19,59 +21,59 @@ node * new_node (int data) {
 
 void insert_at_beggining (node ** root, int data) {
     node *insert_node = new_node (data);
-    
+
     if (*root == NULL) {
-        
+
         *root = insert_node;
-        
+
         return;
-        
+
     }
-    
+
     insert_node->next = *root;
-    
+
     *root = insert_node;
 
 }
 
 void insert_at_middle (node * root, int data, int pos) {
     node *insert_node = new_node (data);
-    
+
     node *curr = root;
-    
+
     node *next;
-    
+
     for (int i = 0; i < pos; i++) {
         curr = curr->next;
     }
     next = curr->next;
-    
+
     curr->next = insert_node;
-    
+
     insert_node = next;
 }
 
 void insert_at_end (node * root, int data) {
     node *insert_node = new_node (data);
-    
+
     node *curr = root;
-    
+
     while (curr->next != NULL) {
-        
+
         curr = curr->next;
-        
+
     }
-    
+
     curr->next = insert_node;
 }
 
 void delete_at_beggining (node * root) {
     node *next = root->next;
-    
+
     node *prev = root;
-    
+
     root = next;
-    
+
     free (prev);
 }
 
@@ -98,44 +100,44 @@ void delete_at_end (node * root) {
 
 void print_nodes (node * root) {
     node *curr = root;
-    
+
     while (curr != NULL) {
-        
+
         cout << curr->data << " ";
-        
+
         curr=curr->next;
-        
+
     }
 
 }
 
 int main () {
     node *root = NULL;
-    
+
     insert_at_beggining (&root, 1);
-    
+
     insert_at_beggining (&root, 2);
-    
+
     insert_at_beggining (&root, 3);
-    
+
     insert_at_beggining (&root, 4);
-    
+
     insert_at_beggining (&root, 5);
-    
+
     insert_at_middle (root, 6, 2);
-    
+
     insert_at_end (root, 7);
-    
+
     print_nodes (root);
-    
+
     delete_at_beggining (root);
-    
+
     delete_at_middle (root, 3);
-    
+
     delete_at_end (root);
-    
+
     print_nodes (root);
-    
+
     return 0;
 
 }
